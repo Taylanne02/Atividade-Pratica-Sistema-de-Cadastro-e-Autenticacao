0cd5a69c53b93d1c4d62473c76c429912641432b
Taylanne <taylannecastelobrancocavalcant@gmail.com>
2026-06-17 12:46:22 -0300

    Teste CORS

--- Backend/server.js
+++ Backend/server.js
@@ -12,15 +12,7 @@ const app = express();
 app.use(express.json());
 
 
-app.use(cors({
-    origin: [
-        "https://atividade-pratica-sistema-cadastro-autenticacao-h05tuvqfq.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
-
-app.options(/.*/, cors());
+app.use(cors());
 
 app.use(routes);
 
